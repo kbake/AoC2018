058dc8ed73112daf23f0ff331318b33ff7f59ed3
kbake <kbake@outlook.com>
2018-12-02 12:11:04 -0500

    Day 2 done, was overcomplicating part 2

---
+++ PythonSolutions/Day2.py
@@ -0,0 +1,310 @@
+puzzle_input = """umdryebvlapkozostecnihjexg
+amdryebalapkozfstwcnrhjqxg
+umdcyebvlapaozfstwcnihjqgg
+ymdryrbvlapkozfstwcuihjqxg
+umdrsebvlapkozxstwcnihjqig
+umdryibvlapkohfstwcnfhjqxg
+umdryebvqapkozfatwcnihjqxs
+umzrpebvlapkozfshwcnihjqxg
+fmhryebvlapkozfstwckihjqxg
+umdryebvlahkozfstwcnizjrxg
+qmdryebvlapkozfslwcnihgqxg
+umdiyebjlapknzfstwcnihjqxg
+umdryebvlapkoqfstwcaihvqxg
+cmdryebvlapkpzfstwcnihjvxg
+umdryebvlakkozfstwcgihjixg
+umdryebvlasjozfstwcnihqqxg
+umdryebvladkozfsvwcnifjqxg
+umdrlebvlapaozfstwcniwjqxg
+umdryebvlhpkozrstwsnihjqxg
+umdryebvcapkozfqtwcnihjrxg
+ubdrykbvlapkowfstwcnihjqxg
+umdryebvldpkozfstwcnihtqsg
+umdryebvlapaozyutwcnihjqxg
+umdryibvlapkozfstdfnihjqxg
+umdryebvlapgozkstwznihjqxg
+umdrxebvlapkozfstwcngxjqxg
+umdryekvlapkozfstwclchjqxg
+nmdryebvlapkozjsewcnihjqxg
+umdryebvyapkozfstfcniheqxg
+umdfyebvlapkozfstwcnhhjpxg
+umdryelylupkozfstwcnihjqxg
+smdryebvlqpkozfstwcnihjdxg
+umdryebvlapaozfsuwcnihjqxc
+umdryebvlrzkozrstwcnihjqxg
+umdbycbvlapkojfstwcnihjqxg
+umdryebvlapkonfstwpnirjqxg
+uecryebvlapkozfstwcnihpqxg
+uqdryebvltpkozfstwcnihrqxg
+umdryebvlqsknzfstwcnihjqxg
+cmdryebvlapkocfstwcvihjqxg
+umdrkebvlapkozqsfwcnihjqxg
+umdryabveapkoifstwcnihjqxg
+ummrnehvlapkozfstwcnihjqxg
+umdryebvlxpkozfstwqnihjtxg
+umdryebvlagkozastwcnihjqxh
+umdryebvlatkozzhtwcnihjqxg
+umdryebvlcpkozfstwrnihjqvg
+umdryebvlapkozfsnwcnrhjcxg
+umdzyebvlypkozfstwcnibjqxg
+nmdryebvlvpkozbstwcnihjqxg
+uwdryebvlipkozfstwcnihvqxg
+umdraebvlavkozfstwcnihjqwg
+umdeyebvlspbozfstwcnihjqxg
+umdryxlvlapkozfstwcnihjqxu
+umdryegvlapkqzfstwcnirjqxg
+umdrupbvlapkozfstwcnihjqog
+imxryebvlapkxzfstwcnihjqxg
+umdrfebvlapkozowtwcnihjqxg
+umdreebvlapkozmstwczihjqxg
+undryebdlapkozbstwcnihjqxg
+umdryebvlapkpzfetwcnihjqxb
+ymdnyebvlapkozfstwinihjqxg
+umdryebvaapkozfstwcnihyqqg
+umdryebvlapkzzwsrwcnihjqxg
+umdrkebvlapkmzfskwcnihjqxg
+umdrmebvlapkozfsvwcnidjqxg
+umdlyehvlapkozfstwcnihjqkg
+umnryebvlrpkozfstwjnihjqxg
+uqdryebvlapxozfsawcnihjqxg
+vmdruebvlapkozfstwcnihjqqg
+umdryabviapkozistwcnihjqxg
+umdryebvlapkzzfstwfnihkqxg
+uvdryebvlapkozfsxwcuihjqxg
+umdlhebvlapkozfstwcnvhjqxg
+umdreebvlapkopfstjcnihjqxg
+umdryebvlazkomfstwynihjqxg
+kmdryebulapkoznstwcnihjqxg
+umdryebvxakkozfstwinihjqxg
+ukdryobvlapkozistwcnihjqxg
+umdryebveapkozfstwcnthjqgg
+mmdrtebvlapcozfstwcnihjqxg
+umdryebvlapkolistwnnihjqxg
+umdryebxlapkozfatwcnihjqxx
+uxdryebvlapkozfstwhniheqxg
+ufdryebvzapkozfstwcnbhjqxg
+amdryhbvlapkozfstwcnifjqxg
+umqryebvlaphozfstwcnihjqxn
+umdryebvlapkosfstfcnihjqxe
+gmkryebvlapkozfstwcnihjmxg
+umdrnebvlkpkozfstwcnihjnxg
+umdryebvrapkozfstmcndhjqxg
+umdryebvmapkozfstichihjqxg
+umdryesvnapkozestwcnihjqxg
+umeryhbvlapkozfstfcnihjqxg
+umdryedvbapkozfstwcnihqqxg
+umdryebllapzozfstwcnihjvxg
+umdcyebvlzdkozfstwcnihjqxg
+umdrybbvlapkbvfstwcnihjqxg
+umdrytbglapkozfsthcnihjqxg
+umdryebvlkpkozfsteclihjqxg
+umdntebvlapkmzfstwcnihjqxg
+lkdryebveapkozfstwcnihjqxg
+ymdryubvlapkozfstwbnihjqxg
+tmrryebvlapkozfstwcnqhjqxg
+umdryeovlaekonfstwcnihjqxg
+umiryeuvlapkozfstwcnihjwxg
+umdryebvlspvozwstwcnihjqxg
+umdrtebvlapkoznxtwcnihjqxg
+umvryebvlaphozfstwcnahjqxg
+umdryebvlapkozfstinniajqxg
+umdryebqlapkozfctwcnihjqxx
+umdryebvlapkbzfptwcnihjqvg
+umdryabviapkozistwcnihjqxd
+umdryrbvlapkezfstscnihjqxg
+umhryebvlapkozfstacnihxqxg
+umdxyelvlapkozfitwcnihjqxg
+umdryevvuapkozfstwcnihtqxg
+uydrypbvxapkozfstwcnihjqxg
+umdryebvlapkopfstwcnihzqxo
+uedryebvlapkozistwceihjqxg
+umdiyebvlapkozfgtwcnihjqxv
+ymdryebvlapkozfsticniqjqxg
+umbrkebvlapkozfslwcnihjqxg
+umdryebliapkozbstwcnihjqxg
+umvryebolapkozfstwcnihjqig
+umdryeavbackozfstwcnihjqxg
+umdryfbvlapsozfstwcnihaqxg
+umdqyebvlapkozfjtgcnihjqxg
+umdrjebvlaqkozfstwcyihjqxg
+umdryebklaqkozrstwcnihjqxg
+umdryebvpapkozfstwcpihjqjg
+uydryebhlawkozfstwcnihjqxg
+umdyyebvlapkozfstwcykhjqxg
+umdryebvlapkozfstwcnitjnxh
+umdzyebvlapkozfstwcnehyqxg
+mmcryebvlapkozfstwinihjqxg
+umdryebvlapuozfstwmvihjqxg
+umdryfbvlapkozqstwcnihjmxg
+umdryebslapsozfhtwcnihjqxg
+umdtyemvlapmozfstwcnihjqxg
+umdrxevvlapkozfytwcnihjqxg
+umdahebvlapjozfstwcnihjqxg
+umdryebvlapkozfstacnivjqxb
+umdryebvlzpkozfjtwcnihjyxg
+umdryebvlaqkozfstwcnisjqxu
+umdrydbvlapkozfsuwcnihjlxg
+umdryebvlapkomrstwcnihjqkg
+umdryebvlapcozfstmcnwhjqxg
+umdryebvlahkozfstwcibhjqxg
+gmdrzebvlapkozlstwcnihjqxg
+umdryebvlapkezfsswcnrhjqxg
+umdryebvlapkoqfitwcgihjqxg
+umdrnebvlapkozfsiwcninjqxg
+umdryebvlapkozfsrwckohjqxg
+umdryebtlapkomfstwcnihjexg
+umdryxbvlapjozfstwcnihoqxg
+umdpyebvlapkosustwcnihjqxg
+umdryebvlapkvzfawwcnihjqxg
+umhnyebvlaikozfstwcnihjqxg
+umdryebvlagkozfstvknihjqxg
+uodryebjlapkoxfstwcnihjqxg
+umdryefdlapkozfstwcnyhjqxg
+umprmebvtapkozfstwcnihjqxg
+umdhyebvlapoozfstwcnihjqgg
+uddryebvidpkozfstwcnihjqxg
+umdryebtlapkozfetwfnihjqxg
+umdbyebolapkozfstwcoihjqxg
+umdryebvlapkonfstwcnihjpxo
+umdryebvlapkohfstwcnihjqwk
+umdryebolalkkzfstwcnihjqxg
+updryebvxapkozfstwcnshjqxg
+umdryebvlapkovfktwcnuhjqxg
+umdrqrbvlppkozfstwcnihjqxg
+umdrylgvlapkozfstwrnihjqxg
+umdryebvlapkozfstxcnihbqig
+uvdryeevlappozfstwcnihjqxg
+zmdryebvlapkozfstwcnihqqxt
+umdryebvlapvozfstwenihiqxg
+umdryebvlbpkozfsgwcnihjlxg
+umdryhbvlapkozfstwcnihtqxw
+umdreecvlapkozwstwcnihjqxg
+umwryebvlapkoztsmwcnihjqxg
+ukdryebvfapkozrstwcnihjqxg
+umdrylbdlamkozfstwcnihjqxg
+umdryebvlapoozwsmwcnihjqxg
+umdryebvlapkozfqtwcnnzjqxg
+umdryekvlapktzfstwcnohjqxg
+umdryebvlapkozfstwcnihjwqo
+umdrrebflapkogfstwcnihjqxg
+umdryevvlapkozfztwctihjqxg
+umdrybbvlapkozfstwcnihxaxg
+umdryebvlapkozfsowcnphjqag
+smdryebvlapbozfitwcnihjqxg
+umdryebvtapiozfstwcnihjqxe
+umdryebjlakkozfstwccihjqxg
+umdryebvlapdozfshwckihjqxg
+umnryebvlapiozfstwcnihlqxg
+umdrycbvlapkjzfsnwcnihjqxg
+umdryebvyaprozjstwcnihjqxg
+ucdryebvlapkozfstwomihjqxg
+umdryebvlagklzfstwcnihjqyg
+umdryebvladkozfstwcnihjqjh
+umdrwebvlapkozfstwdnicjqxg
+umdryebvlapkmzfstwcniheqxr
+umdryebvlapkjzfstwcviheqxg
+umdrvebvlapkozfstwcbihjqmg
+umdrfebvlapkoffstwcnihsqxg
+umdryebvtarkazfstwcnihjqxg
+umdryebvlapkozfstwcfihjcng
+umdryebvlapkktostwcnihjqxg
+uedryeevlapkozfstwcniijqxg
+bmdryebylapkozfstwcnihjqog
+umdryebvlmpkoztstwcnihjqeg
+umdryepvlarkohfstwcnihjqxg
+uwdryebvlapklzfstzcnihjqxg
+umdryebklapkozfsswcbihjqxg
+umdtyeavlapkozfstwsnihjqxg
+umdryebvaapkozfhtfcnihjqxg
+umdrpebvlapuozfstwvnihjqxg
+umdryebvlapkozffmwcniijqxg
+uqdpyebvlapkozfstwfnihjqxg
+umdryebvlapuozdstwcnihjhxg
+tmdryhbvlapkozfptwcnihjqxg
+umdryevvmapkozfstwcnihjgxg
+umdryeuvlapmozfstwcnihjwxg
+umdryebqlzpkozfbtwcnihjqxg
+umdryebvsapkozystwcniqjqxg
+imdryebvlapkozfscwinihjqxg
+umdryebvlzpkozustwcnmhjqxg
+umdrypbvlapbozfsnwcnihjqxg
+bmdryebvlapqoznstwcnihjqxg
+umdrfebvlapaozfstwcnihxqxg
+umdiyebvxapkozfstwcnchjqxg
+umdrygbvlapkozfstwcnizjqxz
+amdryedvlapkozfstwcnihfqxg
+umdryebvvapzozfstwcnihjgxg
+undryebvlapkzzfstjcnihjqxg
+umdryvbvlapgozfrtwcnihjqxg
+umdrkebvlapkozfstwcnihihxg
+umdryebvrppkozfsowcnihjqxg
+umdryebvlapktzfsdwclihjqxg
+otdrdebvlapkozfstwcnihjqxg
+mmdryebvlazkozfxtwcnihjqxg
+umdryebvlapkozfsbwtnihjqxa
+imqryebvrapkozfstwcnihjqxg
+umdryebvlrpkozfscwcnihjqlg
+uedryebvlapkoznsvwcnihjqxg
+umdryebvlqpkozfstscnihjqxj
+umerycbvlapkozfstwcnihjqxh
+umdkykbvlapjozfstwcnihjqxg"""
+
+# Part 1
+test_input = """abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab"""
+
+def hasExactCountOfAny(toCheck, exactCount):
+  found = {}
+  for c in toCheck:
+    if c in found:
+      found[c] += 1
+    else:
+      found[c] = 1
+  for c in found.values():
+    if c == exactCount:
+      return True
+  return False
+
+numTwo   = 0
+numThree = 0
+for word in puzzle_input.splitlines():
+  if hasExactCountOfAny(word, 2):
+    numTwo += 1
+  if hasExactCountOfAny(word, 3):
+    numThree += 1
+print( numTwo * numThree )
+
+# Part 2
+test_input = """abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz"""
+
+def figure_correct_ids(id_list):
+  correct_ids = []
+  for i in range(0, len(id_list)-1):
+    cur_id = id_list[i]
+    next_id = id_list[i+1]
+    num_diff = 0
+    common_letters = ""
+    for x, y in zip(cur_id, next_id):
+      if x == y:
+        common_letters += x
+      else:
+        num_diff += 1
+    if num_diff == 1:
+      correct_ids.append((cur_id, next_id, common_letters))
+  return correct_ids
+
+words = puzzle_input.splitlines()
+words.sort()
+correct_ids = figure_correct_ids(words)
+print(correct_ids)
